d245d5cae78a5d64aaee781b794cdac4d44afb4a
Ishan-Shrestha <sthaishan218@gmail.com>
2026-02-06 11:36:28 +0545

    added the latest modules of tutorial

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,21 +3,27 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
-
+    
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
-
+        # Filter out future questions
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()  # Only questions published in the past or now
+        ).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        # Exclude future questions
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -40,8 +46,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
